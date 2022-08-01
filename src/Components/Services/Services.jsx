47fa2f1d6e10e6d8e850f152ba5732e6d3953362
yuragms <yura.gms@gmail.com>
2022-08-01 18:09:07 +0300

    resume

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,6 +3,8 @@ import "./Services.css";
 import HeartEmoji from "../../img/heartemoji.png";
 import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
+import Card from "../Card/Card";
+import moduleName from "../../";
 
 const Services = () => {
   return (
@@ -16,11 +18,40 @@ const Services = () => {
           <br />
           minima repellat possimus! Accusamus assumenda{" "}
         </span>
-        <button className="button s-button">Download CV</button>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+
         <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
       </div>
       {/* right side */}
-      <div className="cards">I am right side</div>
+      <div className="cards">
+        <div style={{ left: "14rem" }}>
+          <Card
+            emoji={HeartEmoji}
+            heading={"Design"}
+            detail={"Figma, Sketch, Photoshop, Adobe xd"}
+          />
+        </div>
+        {/* second card */}
+        <div style={{ top: "12rem", left: "-4rem" }}>
+          <Card
+            emoji={Glasses}
+            heading={"Developer"}
+            detail={"Html, Css, JavaScript, React"}
+          />
+        </div>
+        {/* 3 card */}
+        <div style={{ top: "19rem", left: "12rem" }}>
+          <Card
+            emoji={Humble}
+            heading={"UI/UX"}
+            detail={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eos qui minima repellat possimus!"
+            }
+          />
+        </div>
+      </div>
     </div>
   );
 };
