a117d2fc9a2becc5949fa47cef05adc2ab8c44b6
yuragms <yura.gms@gmail.com>
2022-08-02 22:06:15 +0300

    swiper

---
+++ src/Components/Portfolio/Portfolio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Portfolio.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const Portfolio = () => {
+  return (
+    <div className="Portfolio">
+      {/* heading */}
+      <span>Recent Projects</span>
+      <span>Portfolio</span>
+    </div>
+  );
+};
+
+export default Portfolio;
