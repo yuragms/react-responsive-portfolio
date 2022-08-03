b33ce10b6ec541108f63a12c77596eeb4046a7ea
yuragms <yura.gms@gmail.com>
2022-08-03 22:10:41 +0300

    emailjs

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Contact.css";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_eqagsbx",
+        "template_3ddtcik",
+        form.current,
+        "BquV6JAOSPz-FU0Vu"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="contact-form">
+      <div className="w-left">
+        <div className="awesome">
+          <span>Get in touch</span>
+          <span>Contact me</span>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+      </div>
+      <div className="c-right">
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="user_name"
+            className="user"
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            name="user_email"
+            className="user"
+            placeholder="Email"
+          />
+          <textarea name="message" className="user" placeholder="Message" />
+          <input type="submit" value="Send" className="button" />
+          <div
+            className="blur c-blur1"
+            style={{ background: "var(--purple)" }}
+          ></div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
