998edf29f9096cebb41b08fb7f2f65e07524ffd8
yuragms <yura.gms@gmail.com>
2022-08-03 14:08:35 +0300

    slider

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Portfolio.css";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Sidebar from "../../img/sidebar.png";
+import Ecommerce from "../../img/ecommerce.png";
+import HOC from "../../img/hoc.png";
+import MusicApp from "../../img/musicapp.png";
+import "swiper/css";
 
 const Portfolio = () => {
   return (
@@ -8,6 +13,10 @@ const Portfolio = () => {
       {/* heading */}
       <span>Recent Projects</span>
       <span>Portfolio</span>
+      {/* slider */}
+      {/* <Swiper>
+        <
+      </Swiper> */}
     </div>
   );
 };
